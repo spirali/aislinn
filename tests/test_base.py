55213fccf6cc4b4801131ba41905cea843fa4ecf
Stanislav Bohm <stanislav.bohm@vsb.cz>
2014-10-21 23:09:34 +0200

    TEST: malloc tests moved from "base" to "mem"

--- tests/test_base.py
+++ tests/test_base.py
@@ -38,29 +38,6 @@ class BaseTests(TestCase):
         self.execute(-1, exitcode=1, stderr=errmsg)
         self.execute(0, exitcode=1, stderr=errmsg)
 
-    def test_heapsize(self):
-        self.program("malloc")
-
-        self.execute(1, ("100",))
-        self.no_errors()
-
-
-        self.execute(1, ("100",), heapsize="1M")
-        self.no_errors()
-
-        self.execute(1,
-                     ("10000000",),
-                     heapsize="1M")
-        self.single_error("heaperror")
-
-        self.execute(1, ("10000000",), heapsize="100M")
-        self.no_errors()
-
-    def test_malloc_free(self):
-        self.program("mallocfree")
-        self.execute(1)
-        self.no_errors()
-
     def test_invalid_args(self):
         self.program("args")
         args = [ "isend_rank_1",
--- tests/test_mem.py
+++ tests/test_mem.py
@@ -17,5 +17,29 @@ class MemTests(TestCase):
         self.execute(1, stdout="")
         self.no_errors()
 
+    def test_heapsize(self):
+        self.program("malloc")
+
+        self.execute(1, ("100",))
+        self.no_errors()
+
+
+        self.execute(1, ("100",), heapsize="1M")
+        self.no_errors()
+
+        self.execute(1,
+                     ("10000000",),
+                     heapsize="1M")
+        self.single_error("heaperror")
+
+        self.execute(1, ("10000000",), heapsize="100M")
+        self.no_errors()
+
+    def test_malloc_free(self):
+        self.program("mallocfree")
+        self.execute(1)
+        self.no_errors()
+
+
 if __name__ == "__main__":
     unittest.main()
